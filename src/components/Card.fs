2eaf602db7bebf959da447e3fbcfef48fa7775fb
natsukium <tm.tomatoes@gmail.com>
2021-02-05 10:54:27 +0900

    Update articles UI

--- src/components/Card.fs
+++ src/components/Card.fs
@@ -14,14 +14,20 @@ let Card
     ReactBindings.React.forwardRef
         (fun (p: {| onClick: Browser.Types.MouseEvent -> unit
                     href: string |}) (ref: IRefValue<Browser.Types.HTMLElement> option) ->
-            Html.a [ prop.className [ Style.``min-w-0``
-                                      Style.``p-4``
-                                      Style.``rounded-lg``
-                                      Style.``shadow-sm`` ]
-                     prop.href p.href
-                     prop.onClick p.onClick
-                     prop.ref !!ref
-                     prop.children [ Html.h4 [ prop.className [ Style.``mb-4`` ]
-                                               prop.text props.title ]
-                                     Html.p props.description
-                                     Html.p props.date ] ])
+            Html.div [ prop.className [ Style.``bg-nord5``
+                                        Style.``p-4``
+                                        Style.``rounded-lg``
+                                        Style.``md:max-w-screen-md``
+                                        Style.``ring-1``
+                                        Style.``ring-nord4``
+                                        Style.``ring-opacity-5`` ]
+                       prop.children [ Html.a [ prop.href p.href
+                                                prop.onClick p.onClick
+                                                prop.ref !!ref
+                                                prop.children [ Html.div [ prop.className [ Style.``mb-4``
+                                                                                            Style.``text-xl``
+                                                                                            Style.``font-bold`` ]
+                                                                           prop.text props.title ]
+                                                                Html.p props.description
+                                                                Html.p [ prop.className [ Style.``text-right`` ]
+                                                                         prop.text props.date ] ] ] ] ])
--- src/pages/blog/index.fs
+++ src/pages/blog/index.fs
@@ -20,35 +20,38 @@ let getStaticProps () =
 let Posts (props: {| allPostsData: array<obj> |}) =
     Layout
         {| children =
-               [ Html.div [ Head [ Html.title (Html.text (sprintf "Articles | %s" config.blogTitle)) ]
-                            Html.h1 [ prop.className ""
-                                      prop.text "Articles" ]
-                            Html.ul [ prop.className ""
-                                      prop.children (
-                                          props.allPostsData
-                                          |> Array.map
-                                              (fun (data: obj) ->
-                                                  Html.li [ prop.className ""
-                                                            prop.key (data?id: string)
-                                                            prop.children [ Link.link (
-                                                                                [ Link.href (
-                                                                                    sprintf
-                                                                                        "/blog/%s/%s"
-                                                                                        (data?id: string)
-                                                                                        (data?slug: string)
-                                                                                  )
-                                                                                  Link.passHref ],
-                                                                                Link.children (
-                                                                                    Card
-                                                                                        {| title = data?title
-                                                                                           description =
-                                                                                               data?description
-                                                                                           date =
-                                                                                               data?published?slice (0,
-                                                                                                                     10) |}
-                                                                                )
-                                                                            ) ] ])
-                                      ) ] ] ] |}
+               [ Html.div [ prop.className [ Style.``md:max-w-screen-md``
+                                             Style.``mx-auto`` ]
+                            prop.children [ Head [ Html.title (Html.text (sprintf "Articles | %s" config.blogTitle)) ]
+                                            Html.h1 [ prop.className ""
+                                                      prop.text "Articles" ]
+                                            Html.ul [ prop.className ""
+                                                      prop.children (
+                                                          props.allPostsData
+                                                          |> Array.map
+                                                              (fun (data: obj) ->
+                                                                  Html.li [ prop.className [ Style.``my-2`` ]
+                                                                            prop.key (data?id: string)
+                                                                            prop.children [ Link.link (
+                                                                                                [ Link.href (
+                                                                                                    sprintf
+                                                                                                        "/blog/%s/%s"
+                                                                                                        (data?id: string)
+                                                                                                        (data?slug: string)
+                                                                                                  )
+                                                                                                  Link.passHref ],
+                                                                                                Link.children (
+                                                                                                    Card
+                                                                                                        {| title =
+                                                                                                               data?title
+                                                                                                           description =
+                                                                                                               data?description
+                                                                                                           date =
+                                                                                                               data?published?slice (0,
+                                                                                                                                     10) |}
+                                                                                                )
+                                                                                            ) ] ])
+                                                      ) ] ] ] ] |}
 
 let main = Posts
 
