498966113f0b8698673c93b06a5b8d8eb82e843d
natsukium <tm.tomatoes@gmail.com>
2021-02-10 20:27:21 +0900

    Update top page

--- src/pages/index.fs
+++ src/pages/index.fs
@@ -1,76 +1,61 @@
-module App
+﻿module App
 
 open Fable.Core.JsInterop
 open Feliz
-open Zanaptak.TypedCssClasses
 
 open Config
 open Next
 open Style
 
 [<ReactComponent>]
-let Card
-    (props: {| href: string
-               title: string
-               text: string |})
-    =
-    Html.a [ prop.href props.href
-             prop.className [ Style.card ]
-             prop.children [ Html.h3 props.title
-                             Html.p props.text ] ]
-
-[<ReactComponent>]
 let Home () =
-    Html.div [ prop.className [ Style.container ]
-               prop.children [ Head [ Html.title (Html.text config.blogTitle)
-                                      Html.link [ prop.rel "icon"
-                                                  prop.href "/favicon.ico" ] ]
-                               Html.main [ prop.className [ Style.main ]
-                                           prop.children [ Html.h1 [ prop.className [ Style.title ]
-                                                                     prop.children [ Html.text "Welcom to "
-                                                                                     Html.a [ prop.href
-                                                                                                  "https://nextjs.org"
-                                                                                              prop.text "Next.js!" ] ] ]
-                                                           Html.p [ prop.className [ Style.description ]
-                                                                    prop.children [ Html.text "Get started by editing  "
-                                                                                    Html.code [ prop.className
-                                                                                                    Style.code
-                                                                                                prop.text
-                                                                                                    "src/pages/index.fs" ] ] ]
-                                                           Html.div [ prop.className [ Style.grid ]
-                                                                      prop.children [ Card
-                                                                                          {| href =
-                                                                                                 "https://nextjs.org/docs"
-                                                                                             title = "Documentation"
-                                                                                             text =
-                                                                                                 "Find in-depth information about Next.js features and API." |}
-                                                                                      Card
-                                                                                          {| href =
-                                                                                                 "https://nextjs.org/learn"
-                                                                                             title = "Learn"
-                                                                                             text =
-                                                                                                 "Learn about Next.js in an interactive course with quizzes!" |}
-                                                                                      Card
-                                                                                          {| href =
-                                                                                                 "https://github.com/vercel/next.js/tree/master/examples"
-                                                                                             title = "Examples"
-                                                                                             text =
-                                                                                                 "Discover and deploy boilerplate example Next.js projects." |}
-                                                                                      Card
-                                                                                          {| href =
-                                                                                                 "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                                                                                             title = "Deploy"
-                                                                                             text =
-                                                                                                 "Instantly deploy your Next.js site to a public URL with Vercel." |} ] ] ] ]
-                               Html.footer [ prop.className Style.footer
-                                             prop.children [ Html.a [ prop.href
-                                                                          "https://vercel.com?utm_source=vreate-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                                                                      prop.target "_blank"
-                                                                      prop.rel "noopener noreferrer"
-                                                                      prop.children [ Html.text "Powered by "
-                                                                                      Html.img [ prop.src "/vercel.svg"
-                                                                                                 prop.alt "Vercel Logo"
-                                                                                                 prop.className
-                                                                                                     Style.logo ] ] ] ] ] ] ]
+    Html.div [ Head [ Html.title (Html.text config.blogTitle)
+                      Html.link [ prop.rel "icon"
 
-Home |> exportDefault
+                                  prop.href "/favicon.ico" ]
+                      Html.link [ prop.rel "stylesheet"
+                                  prop.href "/static/styles/icomoon/style.css" ] ]
+               Html.main [ prop.className [ Style.``h-screen``
+                                            Style.flex
+                                            Style.``flex-col``
+                                            Style.``md:flex-row``
+                                            Style.``md:justify-evenly`` ]
+                           prop.children [ Html.div [ prop.className [ Style.``text-center``
+                                                                       Style.``my-auto`` ]
+                                                      prop.children [ Html.h1 [ prop.className [ Style.``mb-4`` ]
+                                                                                prop.text "Natsukium" ]
+                                                                      Html.p [ prop.className [ "" ]
+                                                                               prop.text "Machine Learning Engineer" ]
+                                                                      Html.p [ prop.className [ "" ]
+                                                                               prop.text "(Bio|Chem)informatician" ]
+                                                                      Html.div [ prop.className [ Style.``mt-4`` ]
+                                                                                 prop.children [ Html.a [ prop.href
+                                                                                                              "https://twitter.com/Pavane1899"
+                                                                                                          prop.children [ Html.span [ Html.i [ prop.className [ "icon-twitter"
+                                                                                                                                                                Style.``mx-1``
+                                                                                                                                                                Style.``hover:text-nord8`` ] ] ] ] ]
+                                                                                                 Html.a [ prop.href
+                                                                                                              "https://github.com/natsukium"
+                                                                                                          prop.children [ Html.span [ Html.i [ prop.className [ "icon-github"
+                                                                                                                                                                Style.``mx-1``
+                                                                                                                                                                Style.``hover:text-nord8`` ] ] ] ] ] ] ] ] ]
+                                           Html.div [ prop.className [ Style.``text-center``
+                                                                       Style.``my-auto`` ]
+                                                      prop.children [ Html.div [ Html.h2 [ prop.className [ Style.``text-3xl``
+                                                                                                            Style.``my-4``
+                                                                                                            Style.``py-4`` ]
+                                                                                           prop.text "About" ] ]
+                                                                      Html.div [ Link.link (
+                                                                                     [ Link.href "/blog"
+                                                                                       Link.passHref ],
+
+                                                                                     Html.a [ Html.h2 [ prop.className [ Style.``text-3xl``
+                                                                                                                         Style.``hover:text-nord8``
+                                                                                                                         Style.``my-4``
+                                                                                                                         Style.``py-4`` ]
+                                                                                                        prop.text "Blog" ] ]
+                                                                                 ) ] ] ] ] ] ]
+
+let main = Home
+
+main |> exportDefault
